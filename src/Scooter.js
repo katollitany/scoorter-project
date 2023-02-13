21e9c2dfda211fc958d2a0b002923866fb692b58
katollitany <katollitany@hotmail.com>
2023-02-13 11:17:22 +0000

    scooter class almost complete

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,30 @@
+const User = require("./User")
+
 class Scooter{
   // scooter code here
+  constructor(station){
+    this.station = station
+    this.user = user
+    this.serial = serial
+    this.charge = charge
+    this.isBroken = isBroken
+    this.nextSerial = []
+  }
+  rent(charge,isBroken){
+    if (charge > 20 && isBroken===false){
+      this.station = null
+      this.user = new User
+    }
+    if (charge <= 20){
+      throw new Error("scooter needs to charge")
+    } 
+    if (isBroken===true){
+      throw new Error("scooter needs repair")
+    }
+  }
+
+  dock(){}
+
 }
 
 
